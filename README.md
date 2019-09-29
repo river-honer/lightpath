2b8e6767ccdb2f53774ce307c9c8171add3d1f42
River Honer <river@someserver.de>
2019-09-29 19:56:31 +0100

    top level

--- README.md
+++ README.md
@@ -1,22 +1,29 @@
-# LightPath Frontend
+# lightpath-fe
 
-## Abstract
-LightPath is an application used to find routes through the city that are well lit, to avoid dark pathways. The concept came from discussions I saw on twitter hoping for this problem to be solved by particularly women and queer people. The concept originally was to use computer vision along with Open Street Maps data to automate the classification of brightness. In the time we had, we were not able to implement the computer vision aspect, but we did make a custom routing algorithm which does use the light data from OSM to find routes that do not go down unlit roads. Our algorithm can also be quite easily adapted for finding the brightest route once we have the data available.
-## Hackathon topic you used
-Accessibility
-## The development tools used to build the project
-- Golang (Backend)
-- Vue.js (Frontend)
+## Project setup
+```
+npm install
+```
 
-## APIs used in the project
-- OPENCAGE (geolocation)
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
 
-## Any libraries used in the project
-- Leaflet (Frontend Maps)
-- Stamen (Map Tiles)
-- Vuetify (Component Library)
-## Licence/s
-MIT
+### Compiles and minifies for production
+```
+npm run build
+```
 
-# LightPath Backend
-The backend is a go project who's github is here [https://github.com/cryptoqween/we-travel-backend](https://github.com/cryptoqween/we-travel-backend)
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HomePage.vue
@@ -0,0 +1,73 @@
+<template>
+    <v-container>
+        <div v-if="messages.length > 0">
+          <v-alert
+            v-for="message in messages"
+            v-model="message.display"
+            :key="message.key"
+            :type="message.type"
+            :dismissible="true"
+          >
+            {{ message.content }}
+          </v-alert>
+        </div>
+        <input-fields
+          :user-coords="userCoords"
+          v-on:submit-form="getData"
+          v-on:message="makeMessage"
+        />
+        <map-view
+          :user-coords="userCoords"
+          :data="data"
+          v-on:message="makeMessage"
+        />
+    </v-container>
+</template>
+
+<script>
+import InputFields from './InputFields';
+import MapView from './MapView';
+import Route from '../models/route';
+import Webapi from '../webapi';
+
+export default {
+  name: 'home-page',
+  components: {
+    InputFields,
+    MapView,
+  },
+  data: () => ({
+    data: undefined,
+    userCoords: null,
+    messages: [],
+  }),
+  mounted() {
+    this.getUserCoords();
+  },
+  methods: {
+    async getData(params) {
+      try {
+        const bodyJson = await Webapi.findPath(params)
+        this.data = new Route(bodyJson);
+      }
+      catch {
+        this.makeMessage({type: 'error', content: 'Could not find route'})
+      }
+    },
+    async getUserCoords() {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const coordArray = [position.coords.latitude, position.coords.longitude];
+        this.userCoords = coordArray;
+      });
+    },
+    makeMessage(options) {
+      this.messages.push({
+        key: this.messages.length,
+        display: true,
+        type: options.type,
+        content: options.content,
+      });
+    },
+  },
+};
+</script>
