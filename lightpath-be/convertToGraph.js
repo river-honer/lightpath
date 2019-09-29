650933ed7b56a2d81a16e375ee021e0df34e8063
River Honer <river@someserver.de>
2019-09-29 12:16:01 +0100

    rm be

--- lightpath-be/convertToGraph.js
+++
@@ -1,4 +0,0 @@
-const Parser = require("geojson-path-finder");
-const geojson = require("./london.geo.json");
-const pathFinder = new Parser(geojson);
-console.log(JSON.stringify(pathFinder._graph, null, 2));
--- lightpath-fe/src/components/MapView.vue
+++ lightpath-fe/src/components/MapView.vue
@@ -52,7 +52,7 @@ export default {
 				const basemap = L.tileLayer(url, {
 					subdomains: ['','a.','b.','c.','d.'],
 					minZoom: 0,
-					maxZoom: 17,
+					maxZoom: 16,
 					type: 'png',
 					attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>'
 				});
